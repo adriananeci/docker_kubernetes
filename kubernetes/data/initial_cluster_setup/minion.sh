bedbf3375da6c596cf1b08bb30390eabcbd931e7
Victor Varza <victor.varza@gmail.com>
2018-02-23 20:45:59 +0200

    kubernetes - refactoring

--- kubernetes/data/initial_cluster_setup/minion.sh
+++ kubernetes/data/initial_cluster_setup/minion.sh
@@ -6,7 +6,7 @@ cluster_join=""
 while [ "$cluster_join" == "" ] ; do
     echo "waiting for the master node";
     sleep 1;
-    cluster_join="$(cat /data/.kubeadmin_init  | grep 'kubeadm join')"
+    cluster_join="$(cat /data/initial_cluster_setup/.kubeadmin_init  | grep 'kubeadm join')"
 done
 
 $cluster_join
